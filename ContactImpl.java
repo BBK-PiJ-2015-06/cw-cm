dc9d2b629f6017c029edf8845fae4f19d59f4ac9
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-16 16:13:16 +0000

    ContactImpl class created. ContactImpl designed to implement interface Contact. Structure added to class to ensure that ContactImpl and TestContact both compile correctly. Test currently fails after running junit.

---
+++ ContactImpl.java
@@ -0,0 +1,22 @@
+public class ContactImpl implements Contact {
+	
+	public ContactImpl(int id, String name, String notes) {}
+	
+	@Override
+	public int getId() {
+		return -1;
+	}
+	
+	@Override
+	public String getName() {
+		return null;
+	}
+	
+	@Override
+	public String getNotes() {
+		return null;
+	}
+	
+	@Override
+	public void addNotes(String note) {}
+}
