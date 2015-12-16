0c2c31b1e9c0dc4e952336fe127728732d7cb287
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-16 16:19:34 +0000

    General constructor created for ContactImpl and body added to getter methods. All tests now pass.

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,20 +1,28 @@
 public class ContactImpl implements Contact {
 	
-	public ContactImpl(int id, String name, String notes) {}
+	private int id;
+	private String name;
+	private String notes;
+	
+	public ContactImpl(int id, String name, String notes) {
+		this.id = id;
+		this.name = name;
+		this.notes = notes;
+	}
 	
 	@Override
 	public int getId() {
-		return -1;
+		return this.id;
 	}
 	
 	@Override
 	public String getName() {
-		return null;
+		return this.name;
 	}
 	
 	@Override
 	public String getNotes() {
-		return null;
+		return this.notes;
 	}
 	
 	@Override
