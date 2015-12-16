3337dfec4796a38ae193a80426bd834cda3bf62f
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-16 16:06:49 +0000

    TestContact unit test class created. First test created, which tests that getter methods work correctly on object created with general constructor.

---
+++ TestContact.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TestContact {
+	
+	@Test 
+	public void testGetterMethodsWithGeneralConstructor() {
+		Contact myContact = new ContactImpl(3, "Harry Kane", "Awesome striker");
+		assertEquals(3, myContact.getId());
+		assertEquals("Harry Kane", myContact.getName());
+		assertEquals("Awesome Striker", myContact.getNotes());
+	}
+	
+}
