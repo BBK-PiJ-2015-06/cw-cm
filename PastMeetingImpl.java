2125b0fe404b4aa8007638d1d399493348b0a71b
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-17 14:10:46 +0000

    JavaDoc comments added to PastMeetingImpl methods and class.

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -1,10 +1,47 @@
 import java.util.Calendar;
 import java.util.Set;
 
+/**
+ * A PastMeetingImpl is an implementation of the interface PastMeeting, 
+ * which is defined in section 3.4 of the document for Assignment 
+ * 3 - Contact Manager.
+ *
+ * A PastMeetingImpl is a class that extends MeetinImpl and so inheriting all
+ * of the functionality of this abstract class. PastMeetingImpl provides an 
+ * additional field 'notes' and one additional method getNotes() enabling objects
+ * of this class to store and retrieve notes made about meetings that have already
+ * occured.
+ *
+ * @author George Shiangoli
+ */
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	
 	private String notes;
 	
+	/**
+	 * The constructor of PastMeetingImpl accepts four parameters: the ID and date
+	 * of the meeting, the Set of contacts that attend(ed) the meeting and any notes
+	 * that may have been recorded during / after the meeting.
+	 *
+	 * Each parameter is checked for validity in turn, starting with the ID, followed
+	 * by date, contacts and finally notes. No null values can be passed as 
+	 * parameters, the meeting ID must be greater than or equal to 1, and the Set
+	 * of contacts cannot be empty.
+	 *
+	 * Note that since each parameter is checked in the order specified by the constructor
+	 * method's declaration, an IllegalArgumentException will always be thrown if the
+	 * ID provided is invalid, even if the remaining parameters are null and so on.
+	 *
+	 * This method calls the constructor of the parent class MeetingImpl.
+	 *
+	 * @param id the unique ID of the meeting being created
+	 * @param date the date at which the meeting was or will be held
+	 * @param contacts a set of non identical contacts that attend this meeting
+	 * @param notes the notes made about the meeting
+	 * @throws IllegalArgumentException if the ID provided is less than 1 and / or the 
+	 *         Set of contacts is empty
+	 * @throws NullPointerException if any of the parameters are null
+	 */
 	public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String notes) {
 		super(id, date, contacts);
 		if(notes == null) {
@@ -13,6 +50,11 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 		this.notes = notes;
 	}
 	
+	/**
+	 *{@inheritDoc}
+	 *
+	 * @return {@inheritDoc}
+	 */
 	@Override
 	public String getNotes() {
 		return this.notes;
