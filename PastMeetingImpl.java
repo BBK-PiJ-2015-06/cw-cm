a474cba8711732b099fe852f357541eade69d50a
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-17 10:43:58 +0000

    Notes field added to PastMeetingImpl, as well as setting implementation within the constructor, which now in addition to calling the constructor of the parent class, also stores the notes provided. Body added to getNotes() method to ensure all tests now pass.

--- PastMeetingImpl.java
+++ PastMeetingImpl.java
@@ -3,12 +3,15 @@ import java.util.Set;
 
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
 	
+	private String notes;
+	
 	public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String notes) {
 		super(id, date, contacts);
+		this.notes = notes;
 	}
 	
 	@Override
 	public String getNotes() {
-		return null;
+		return this.notes;
 	}
 }
