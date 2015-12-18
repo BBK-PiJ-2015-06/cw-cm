be597cee60d66457be9495c7d3b2ebdfec319cfb
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-18 09:11:27 +0000

    New 'if' statement added to addNewContact(String, String) method to test for empty strings being passed as parameters and throws an IllegalArgumentException if this situation arises. All tests now pass.

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -13,9 +13,12 @@ public class ContactManagerImpl implements ContactManager {
 	
 	@Override
 	public int addNewContact(String name, String notes) {
+		if(name.equals("") || notes.equals("")) {
+			throw new IllegalArgumentException("Parameters cannot be empty strings");
+		}
 		int newContactId = this.contacts.size() + 1;
 		Contact newContact = new ContactImpl(newContactId, name, notes);
 		contacts.add(newContact);
-		return newContactId;
+		return newContact.getId();
 	}
 }
