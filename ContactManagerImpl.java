b97b905ce01cd069abab29e6e8ec9b997036e9f6
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-18 09:02:01 +0000

    Adjustments made to ContactManagerImpl to ensure new test passes. A new private field has been created of type Set<Contacts> that stores all contacts added to the CM. Body added to addNewContact(String, String) method that creates a new contact and adds it to this set. All tests now pass.

--- ContactManagerImpl.java
+++ ContactManagerImpl.java
@@ -1,15 +1,21 @@
 import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
+import java.util.HashSet;
 
 public class ContactManagerImpl implements ContactManager {
 	
+	private Set<Contact> contacts;
+	
 	public ContactManagerImpl() {
-		
+		this.contacts = new HashSet<Contact>();
 	}
 	
 	@Override
 	public int addNewContact(String name, String notes) {
-		return 1;
+		int newContactId = this.contacts.size() + 1;
+		Contact newContact = new ContactImpl(newContactId, name, notes);
+		contacts.add(newContact);
+		return newContactId;
 	}
 }
