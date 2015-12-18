1d2678b1193b31ef82571cd8735217700a6b346c
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-18 08:40:33 +0000

    New concrete class ContactManagerImpl created and all necessary code and structure added to ensure that testing class compiles correctly. However, latest test fails.

---
+++ ContactManagerImpl.java
@@ -0,0 +1,15 @@
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
+public class ContactManagerImpl implements ContactManager {
+	
+	public ContactManagerImpl() {
+		
+	}
+	
+	@Override
+	public int addNewContact(String name, String notes) {
+		return -1;
+	}
+}
