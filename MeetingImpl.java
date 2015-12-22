bbe1b6ce9094d6a6cfbc3fa0a80f99bd7ebd4b4a
George Shiangoli <gshian01@mail.bbk.ac.uk>
2015-12-22 12:36:12 +0000

    Code within MeetingImpl refactored to avoid code duplication. Constructor now calls ContactManagerUtils.nullParamChecker to check for null values.

--- MeetingImpl.java
+++ MeetingImpl.java
@@ -23,14 +23,11 @@ public abstract class MeetingImpl implements Meeting, Serializable {
 	 * The constructor of MeetingImpl accepts three parameters: the ID and date
 	 * of the meeting, and the Set of contacts that attend(ed) the meeting.
 	 *
-	 * Each parameter is checked for validity in turn, starting with the ID, followed
-	 * by date, and finally with contacts. No null values can be passed as 
-	 * parameters, the meeting ID must be greater than or equal to 1, and the Set
-	 * of contacts cannot be empty.
-	 *
-	 * Note that since each parameter is checked in the order specified by the constructor
-	 * method's declaration, an IllegalArgumentException will always be thrown if the
-	 * ID provided is invalid, even if the remaining parameters are null and so on.
+	 * Each parameter is checked for null values and once it is established that
+	 * no null parameters have been passed, each is then checked for validity in turn, 
+	 * starting with the ID, followed by date, and finally with contacts. No null values 
+	 * can be passed as parameters, the meeting ID must be greater than or equal to 1, 
+	 * and the Set of contacts cannot be empty.
 	 *
 	 * @param id the unique ID of the meeting being created
 	 * @param date the date at which the meeting was or will be held
@@ -40,15 +37,11 @@ public abstract class MeetingImpl implements Meeting, Serializable {
 	 * @throws NullPointerException if any of the parameters are null
 	 */
 	public MeetingImpl(int id, Calendar date, Set<Contact> contacts) {
-		if(id <= 0) {
+		ContactManagerUtils.nullParamChecker(date, contacts);
+		if(id < 1) {
 			throw new IllegalArgumentException("Meeting ID cannot be less than 1");
 		}
-		if(date == null) {
-			throw new NullPointerException("Date cannot be null");
-		}
-		if(contacts == null) {
-			throw new NullPointerException("Set<Contacts> cannot be null");
-		} else if(contacts.isEmpty()) {
+		if(contacts.isEmpty()) {
 			throw new IllegalArgumentException("Set<Contact> must have at least 1 contact");
 		}
 		this.id = id;
